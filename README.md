28f64f88d7b7037da81001a67dc2ef52da59223d
Eduardo Marques <luiseduardo.marques.ds@gmail.com>
2025-06-01 21:15:21 -0300

    proj stock-insights

---
+++ README.md
@@ -0,0 +1,54 @@
+# Projeto DebugVsCode
+
+API em Python com AWS Lambda que retorna o histórico de preços e dados básicos de ações da B3, utilizando `yfinance` e a API pública do Brapi.
+
+---
+
+## 📁 Estrutura do Projeto
+
+```
+- requirements.txt
+- .gitignore
+- README.md
+- serverless.yml
+- .vscode/
+  - launch.json
+- functions/
+  - stock_analysis.py
+```
+
+---
+
+## 🚀 Funcionalidade
+
+A função `fetch_stock_data` exposta via API retorna:
+- Preço histórico (últimos 5 anos)
+- Informações básicas da ação (via brapi.dev)
+
+---
+
+## ✅ Requisitos
+
+- Python 3.11
+- `pip install -r requirements.txt`
+- Token de acesso à API Brapi (opcional para alguns dados)
+
+---
+
+## 🛠 Instalação
+
+1. Clone o repositório:
+```bash
+git clone https://github.com/seu-usuario/DebugVsCode.git
+cd DebugVsCode
+```
+
+2. Instale as dependências:
+```bash
+pip install -r requirements.txt
+```
+
+3. Execute localmente com o Serverless Framework:
+```bash
+sls offline
+```
---
+++ functions/stock_analysis.py
@@ -0,0 +1,62 @@
+from json import dumps
+from logging import error
+from os import environ
+
+import debugpy  # Não esqueça de tirar pós testes
+import pandas as pd
+import yfinance as yf
+from requests import get
+
+# Não esqueça de tirar pós testes
+debugpy.listen(("0.0.0.0", 5678))
+print("⏳ Aguardando debugger...")
+debugpy.wait_for_client()
+# ^^^ Não esqueça de tirar pós testes
+
+
+def fetch_price_history(ticker: str, period: str = "5y"):
+    try:
+        stock = yf.Ticker(ticker)
+        return stock.history(period=period)
+    except Exception as e:
+        error(f"Error fetching price history for {ticker}")
+        raise e
+
+
+def fetch_fundamentals(ticker: str) -> dict:
+    param = {"token": environ.get("TOKEN_BRAPI")}
+    response = get(f"https://brapi.dev/api/quote/{ticker}?range=1d", params=param)
+
+    data = {
+        "statusCode": response.status_code,
+        "body": (
+            response.json()["results"][0]
+            if len(response.content) != 0
+            else response.text
+        ),
+    }
+    return data
+
+
+def build_response(ticker: str, fundamentals: dict, history_df: pd.DataFrame) -> dict:
+    price_history = [
+        {"date": date.strftime("%Y-%m-%d"), "close": float(close)}
+        for date, close in zip(history_df.index, history_df["Close"])
+    ]
+
+    return {
+        "ticker": ticker,
+        "fundamentals": fundamentals,
+        "priceHistory": price_history,
+    }
+
+
+def handler(event: dict, context: dict):
+    ticker_yf = "PETR4.SA"
+    ticker_brapi = "PETR4"
+
+    price_data = fetch_price_history(ticker_yf)
+    response = fetch_fundamentals(ticker_brapi)
+    if response["statusCode"] == 200:
+        response = build_response(ticker_brapi, response["body"], price_data)
+    return {"statusCode": 200, "body": dumps(response)}
